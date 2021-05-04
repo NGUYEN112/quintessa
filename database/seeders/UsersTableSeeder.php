528b9494a4930ab9304be7fc2b695159f8c31b4a
NGUYEN112 <phannguyenhoainguyen@gmail.com>
2021-05-04 13:47:02 +0700

    add seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'full_name' => 'Nguyên',
+            'email' =>'nguyen@email.com',
+            'password' => bcrypt('123123'),
+            'level' => 0,
+            'avatar' => 'images/nguyen-avatar.jpg',
+            'point' => 0,
+        ]);
+
+    }
+}
